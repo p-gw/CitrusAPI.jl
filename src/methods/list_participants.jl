4730cc67c37cadbb3bcf1c31a704d1236e29c962
p-gw <p.gewessler@gmx.at>
2022-11-29 13:08:37 +0100

    set new default values

--- src/methods/list_participants.jl
+++ src/methods/list_participants.jl
@@ -1,10 +1,9 @@
 export list_participants
 
 """
-    list_participants(client, survey_id, start; kwargs...)
+    list_participants(client, survey_id, start=0; kwargs...)
 
 Return a list of participants for the remote survey with `survey_id`.
-A start ID (`start`) must be specified.
 
 ## Keyword arguments
 - `limit`: Number of participants to return (default: `10`)
@@ -14,7 +13,7 @@ A start ID (`start`) must be specified.
 
 See also: <https://api.limesurvey.org/classes/remotecontrol_handle.html#method_list_participants>
 """
-function list_participants(client::CitrusClient, survey_id::Int, start::Int; limit::Int=10, unused::Bool=false, attributes=false, conditions=[])
+function list_participants(client::CitrusClient, survey_id::Int, start::Int=0; limit::Int=10, unused::Bool=false, attributes=false, conditions=[])
     payload = construct_payload("list_participants", [client.session_key, survey_id, start, limit, unused, attributes, conditions])
     response = call_limesurvey_api(client, payload)
     return response
